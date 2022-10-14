515e6c302e65cba17d65bc5522ff2eea7c2b1480
YuriKey <114105131+YuriKey@users.noreply.github.com>
2022-10-15 00:10:04 +0400

    Delete задача 00 directory

--- задача 00/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// комментарий с текстом задачи
-
-
-Console.Clear();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
-
-int sqr = number * number;
-Console.WriteLine($"Квадрат числа {number} равен {sqr}");
