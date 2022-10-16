18fd4ec2b922830f464d398db44e8cbeac8b0926
yuri_k <krenev_yu@mail.ru>
2022-10-16 11:26:50 +0400

    exec_36

--- Задача 34_Чет и нечет в массиве/Program.cs
+++ Задача 34_Чет и нечет в массиве/Program.cs
@@ -2,45 +2,35 @@
 //которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-// функция заполнения массива числами от 100 до 999, принимает размер массива, мин и макс
-int[] GetArray(int size, int minValue, int maxValue) //создаем функцию для создания массива с вручную задаваемым размером size и интервалом от minValue до maxValue
+int[] GetArray(int size)        //Функция, создающая массив, заполненный случайными положительными трёхзначными числами.
 {
-    int[] res = new int[size];                       //создаем результирующий массив для накопления результата
+    int[] res = new int[size];
 
-    for (int i = 0; i < size; i++)                              //Пользуемся циклом for, т.к знаем, сколько будет итераций.
-    {                                                           // Цикл нужен для заполнения массива res случайными значениями
-        res[i] = new Random().Next(minValue, maxValue + 1);     // от minValue до maxValue, включая maxValue, т.к. Next работает
-    }                                                           // по методу полуинтервала.
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+    }
     return res;
 }
 
-// функция, считающая четные числа, принимает массив, перебирает его через фор, проверяет на четность, выдает каунт
-int EvenNumb(int[] someArray)                        // Создаем функцию, считающую элементы, находящиеся в заданном интервале.
+int EvenNumb(int[] someArray)                       // Создаем функцию, считающую количество четных элементов в заданном интервале.
 {
-    int count = 0;                                  //Результирующая переменная-счетчик.
+    int count = 0;
     for (int i = 0; i < someArray.Length; i++)
     {
         if (someArray[i] % 2 == 0)
         {
             count += 1;
         }
-
     }
     return count;
 }
 
-
-
 Console.Clear();
 
-Console.WriteLine("Размер массива: ");
+Console.WriteLine("Введите размер массива: ");
 int sizeArray = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите минимум: ");
-int minArray = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Введите максимум: ");
-int maxArray = int.Parse(Console.ReadLine()!);
-
-int[] newArray = GetArray(sizeArray, minArray, maxArray);
+int[] newArray = GetArray(sizeArray);
+Console.WriteLine(String.Join(", ", newArray));
 Console.WriteLine($"Количество четных чисел в заданном массиве: {EvenNumb(  newArray)}"!);
---
+++ Задача 36_Сумма нечетных/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] GetArray(int size)        //1.Функция задает массив случайных чисел, принимает размер массива, выдает массив.
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next();
+    }
+    return res;
+}
+
+int UnevenPos(int[] someArray)  //2.Функция принимает массив, перебирает элементы, 
+{                               //если элемент стоит на нечетной позиции, складывает в int res.
+    int res = 0;
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            res = res + someArray[i];
+        }
+
+    }
+    return res;
+}
+Console.Clear();
+
+Console.WriteLine("Размер массива: ");
+int sizeArray = int.Parse(Console.ReadLine()!);
+
+int[] array = GetArray(sizeArray);
+
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равна: {UnevenPos(array)}");
