97184569b909586e371d076eb60993c42711d8fc
yuri_k <krenev_yu@mail.ru>
2022-10-16 15:25:30 +0400

    rep_all.4

---
+++ Задача 00_Квадрат числа/Program.cs
@@ -0,0 +1,9 @@
+﻿// комментарий с текстом задачи
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
